5e3a1c6d0b6eb2576af8d4cb5fa207f4cdb22e74
tofutych <martirosov.davidka@mail.ru>
2024-08-01 07:39:48 +0300

    reverse integer

---
+++ reverse_integer.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def reverse(self, x):
+        reversed_x = int(str(abs(x))[::-1])
+        return (
+            (reversed_x if x > 0 else -reversed_x)
+            if reversed_x.bit_length() < 32
+            else 0
+        )
+
+
+def revers(x: int):
+    reversed_x = int(str(abs(x))[::-1])
+    return reversed_x if x > 0 else -reversed_x if reversed_x.bit_length() < 32 else 0
+
+
+print(revers(-10))
