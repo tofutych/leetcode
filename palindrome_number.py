1b192d675a6606d644dbfccfa9d94b45925d5832
tofutych <martirosov.davidka@mail.ru>
2024-08-02 07:40:59 +0300

    some optimisation

--- palindrome_number.py
+++ palindrome_number.py
@@ -2,7 +2,7 @@ class Solution(object):
     def isPalindrome(self, x):
         if x == 0:
             return True
-        if x < 0 or (x > 0 and x % 10 == 0):
+        if x < 0 or x % 10 == 0:
             return False
 
         result = 0
