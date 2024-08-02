7c30258fdddeee2b076fef4eac43af3283505bbc
tofutych <martirosov.davidka@mail.ru>
2024-08-02 07:38:56 +0300

    made it clean

--- reverse_integer.py
+++ reverse_integer.py
@@ -6,11 +6,3 @@ class Solution(object):
             if reversed_x.bit_length() < 32
             else 0
         )
-
-
-def revers(x: int):
-    reversed_x = int(str(abs(x))[::-1])
-    return reversed_x if x > 0 else -reversed_x if reversed_x.bit_length() < 32 else 0
-
-
-print(revers(-10))
