0374f1692d3cf359d5868f685edcb1f10125e9f4
tofutych <martirosov.davidka@mail.ru>
2024-08-05 17:08:15 +1100

    maximum subarray

--- maximum_subarray.py
+++ maximum_subarray.py
@@ -1,10 +1,7 @@
+class Solution:
     def maxSubArray(self, nums):
         newNum = maxTotal = nums[0]
         for i in range(1, len(nums)):
             newNum = max(nums[i], nums[i] + newNum)
             maxTotal = max(newNum, maxTotal)
         return maxTotal
-
-
-nums = [5, 4, -1, 7, 8]
-print(Solution().maxSubArray(nums=nums))
